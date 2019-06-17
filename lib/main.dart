18d60896a60bc4b5f36ec2f47e449fc84570abff
Yasas4D <yasassandeepa007@gmail.com>
2019-06-17 22:44:33 +0530

    thawa text ekk damma

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,10 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
           ],
         ),
       ),
